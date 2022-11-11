233642f7899e4a14fd974b718f7801e91c9fd68b
mahin33ferdous <mahinferdousshoily@gmail.com>
2022-11-11 10:07:44 +0600

    added to cart

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import './Product.css'
 
-const Product = () => {
+const Product = (props) => {
+    const{name,img,seller,ratings,price}=props.product
+    const{handleAddToCart,product}=props
     return (
-        <div>
-            <h1>product</h1>
+        <div className='product'>
+            <img src={img} alt="" />
+            <div className='product-info'>
+            <p className='product-name'>{name}</p>
+            <p>Price: {price}$</p>
+            <p><small>Seller: {seller}</small></p>
+            <p><small>Rating: {ratings} stars</small></p>
+            </div>
+            <button onClick={()=>handleAddToCart(product)}  className='button-cart'>
+                <p> Add To Cart</p>
+            </button>
         </div>
     );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -4,20 +4,35 @@ import './Shop.css'
 
 const Shop = () => {
     const[products, setproducts]=useState([])
+    const[cart,setCart]=useState([]);
+   
+
     useEffect(()=>{
         fetch('products.json')
         .then(res=>res.json())
         .then(data=>setproducts(data))
     }, [])
+    const clickHandler=(product)=>{
+        console.log(product)
+        const newCart=[...cart,product];// copy the previous cart array
+        setCart(newCart);
+    }
     return (
         <div className='shop'>
             <div className="products-container">
                 {
-                    products.map(product=><Product></Product>)
+                    products.map(product=><Product
+                         key={product.id}
+                         product={product}
+                         handleAddToCart={clickHandler}
+                         >
+
+                         </Product>)
                 }
             </div>
             <div className="cart-container">
             <h1>this is cart</h1>
+            <p>product :{cart.length}</p>
             </div>
         </div>
     );
